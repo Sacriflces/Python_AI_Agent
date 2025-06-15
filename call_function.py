1793f5929ab0a488daaa2fa2dfa5ca728300bc48
Sacriflces <Jmhservice@gmail.com>
2025-06-15 13:06:23 -0400

    Removed calculator/pkg/__pycache__. Also introduced a bug for the ai agent to fix in the calculator file.

--- call_function.py
+++ call_function.py
@@ -1,8 +1,8 @@
 from google.genai import types
-from functions.get_files_info import schema_get_files_info
-from functions.get_file_content import schema_get_file_content
-from functions.run_python import schema_run_python_file
-from functions.write_file import schema_write_file
+from functions.get_files_info import schema_get_files_info, get_files_info
+from functions.get_file_content import schema_get_file_content, get_file_content
+from functions.run_python import schema_run_python_file, run_python_file
+from functions.write_file import schema_write_file, write_file
 
 available_functions = types.Tool(
     function_declarations=[
@@ -12,3 +12,42 @@ available_functions = types.Tool(
         schema_write_file
     ]
 )
+
+function_dict = {
+    "get_files_info": get_files_info,
+    "get_file_content": get_file_content,
+    "run_python_file": run_python_file,
+    "write_file": write_file
+}
+
+def call_function(function_call_part, verbose=False):
+    function_name = function_call_part.name
+    function_args = function_call_part.args
+    if verbose:
+        print(f"Calling function: {function_name}({function_args})")
+    else:
+        print(f" - Calling function: {function_name}")
+    if function_name not in function_dict:
+        return types.Content(
+            role="tool",
+            parts=[
+                types.Part.from_function_response(
+                    name=function_name,
+                    response={"error": f"Unknown function: {function_name}"},
+                )
+            ],
+        )
+    function_args["working_directory"] = "./calculator"
+    function_result = function_dict[function_name](**function_args)
+    return types.Content(
+        role="tool",
+        parts=[
+            types.Part.from_function_response(
+                name=function_name,
+                response={"result": function_result},
+            )
+        ],
+    )
+    
+
+    
--- main.py
+++ main.py
@@ -3,32 +3,68 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
-from call_function import available_functions
+from call_function import available_functions, call_function
 from prompts import system_prompt
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-client = genai.Client(api_key=api_key)
-if len(sys.argv) < 2:
-    print("A prompt is needed!")
-    exit(1)
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+    verbose = "--verbose" in sys.argv
+    args = [arg for arg in sys.argv[1:] if not arg.startswith("--")]
+    if not args:
+        print("A prompt is needed!")
+        exit(1)
+    
+    user_prompt = " ".join(args)
 
-user_prompt = sys.argv[1]
-messages = [
-    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
-]
-response = client.models.generate_content(
-    model="gemini-2.0-flash-001", 
-    contents=messages,
-    config=types.GenerateContentConfig(tools=[
-        available_functions],
-        system_instruction=system_prompt),
-    )
+    if verbose:
+        print(f"user prompt: {user_prompt}\n")
 
-print(response.text)
-for function_call_part in response.function_calls:
-    print(f"Calling function: {function_call_part.name}({function_call_part.args})")
-if "--verbose" in sys.argv:
-    print(f"User prompt: {user_prompt}")
-    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-    print("Response tokens:", response.usage_metadata.candidates_token_count)
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+    agent_feedback_loop(client, messages, verbose)
+
+def generate_content(client, messages, verbose):
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001", 
+        contents=messages,
+        config=types.GenerateContentConfig(tools=[
+            available_functions],
+            system_instruction=system_prompt),
+        )
+    
+    if verbose:
+        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        print("Response tokens:", response.usage_metadata.candidates_token_count)
+
+    return response
+        
+
+
+def agent_feedback_loop(client, messages, verbose):
+    for _ in range(20):
+        response = generate_content(client, messages, verbose)
+        if not response.function_calls:
+            print(response.text)
+            return
+        function_call_results = call_functions(response.function_calls, verbose)
+        for candidate in response.candidates:
+            messages.append(candidate.content)
+        for function_call_result in function_call_results:
+            messages.append(function_call_result)
+
+def call_functions(function_calls, verbose):
+    results = []
+    for function_call_part in function_calls:
+        function_call_result = call_function(function_call_part, verbose)
+        if not function_call_result.parts[0].function_response.response:
+            raise Exception("Error: No response from function call.")
+        if verbose:
+            print(f"-> {function_call_result.parts[0].function_response.response['result']}")
+        results.append(function_call_result.parts[0].function_response.response['result'])
+    return results
+
+if __name__ == "__main__":
+    main()
